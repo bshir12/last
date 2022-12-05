7f9fd114e4ff794314d664e61871989b35440e2f
LAPTOP-IBTJD5UC\bshir <bashir.ibs07@gmail.com>
2022-12-05 14:37:32 +0700

    pertama

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -12,7 +12,7 @@ export default function Register() {
               <div className="rounded-t mb-0 px-6 py-6">
                 <div className="text-center mb-3">
                   <h6 className="text-blueGray-500 text-sm font-bold">
-                    Buat Akun Anda untuk mulai Investasi
+                    Buat Akun Anda dan mulai Investasi
                   </h6>
                 </div>
                 <hr className="mt-6 border-b-1 border-blueGray-300" />
@@ -74,6 +74,20 @@ export default function Register() {
                       placeholder="apri31"
                     />
                   </div>
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="password"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="name"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="Apri@Gmail.com"
+                    />
+                  </div>
+                  
                   <div className="text-center mt-6">
                     <button
                       className="bg-blue-500 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
