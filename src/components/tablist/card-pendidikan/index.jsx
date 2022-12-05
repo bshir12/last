387ab79d9d4110a4329eb7abaf4a882fa98c3075
LAPTOP-IBTJD5UC\bshir <bashir.ibs07@gmail.com>
2022-12-05 22:00:44 +0700

    kedua

--- src/components/tablist/card-pendidikan/index.jsx
+++ src/components/tablist/card-pendidikan/index.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import TombolPenjelasan from '../buttondetail'
 
 const CardPendidikan = () => {
+  const [users, setUsers] = useState([])
+
+  const getCardPendidikan = async () => {
+    try {
+      let response = await axios.get(`https://jsonplaceholder.typicode.com/users`)
+      console.log(response.data);
+      setUsers(response.data);
+    } catch {
+      console.log("Error");
+    }
+  }
+  useEffect(() => {
+    getCardPendidikan();
+  }, [])
   return (
-    <div className='w-72 h-80 rounded-lg border shadow-xl bg-sky-400 border-slate-500 hover:bg-slate-400 block'>
-      <img src={require('./child.png')} className='w-40 ml-14'></img>
-      <div className=' w-full h-32'>
-      <a className='text-lg'>Asuransi Jiwa BRI Assurance</a>
-      <p className='text-xs'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus rerum nisi mollitia eaque tempora. Neque in sapiente laboriosam at tempore nisi incidunt, excepturi dolorem fugit eaque, rerum similique veniam unde.</p>
-      <p className='mt-2 font-semibold'>Rp.55.000,-/Bulan</p>
-      <TombolPenjelasan/>
+    <>
+      <div className='grid grid-rows-auto grid-cols-4 gap-4'>
+        {users.map(user=>(
+          <div key={user.id}>
+            <div className='w-72 h-80 rounded-lg border shadow-xl bg-sky-400 border-slate-500'>
+              <img src={require('./child.png')} className='w-40 ml-14' alt='child'></img>
+              <div className=' w-full h-32'>
+                <a className='text-lg'>{user.name}</a>
+                <p className='text-xs'>{user.username}</p>
+                <p className='mt-2 font-semibold'>{user.email}</p>
+              </div>
+            </div>
+            <TombolPenjelasan/>
+          </div>
+        ))}
       </div>
-    </div>
+    </>
   )
 }
 
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -6,132 +6,92 @@ const Profile = () => {
   return (
     <>
       <Header />
-      <div className="bg-slate-400 m-6 rounded-lg h-60">
-        <div className="flex justify-left">
-          <img
-            className=" w-48 h-48 ml-12 mt-5 rounded-full border "
-            src="http://placeimg.com/640/360/any"
-            alt="#"
-          />
-
-          <div className="block ml-9 mt-4">
-            {/* Full Name */}
-            <div>
-              <label className="form-label inline-block mb-2 text-gray-700">
-                Nama Lengkap
-              </label>
-            </div>
-            <div className="flex">
-              <input
-                className=" border border-solid border-gray-300 rounded-md"
-                placeholder="  Anak Ilang"
-              />
-              <input
-                className=" ml-6 w-12 text-center border border-solid border-gray-300 rounded-md"
-                placeholder="Umur"
-              />
-            </div>
-            {/* Phone Number */}
-            <div>
-              <label className="form-label inline-block mb-2 text-gray-700">
-                Phone Number (+62)
-              </label>
-            </div>
-            <div className="flex">
-              <input
-                className=" border border-solid border-gray-300 rounded-md"
-                placeholder="  081213141516"
-              />
-            </div>
-            {/* Alamat */}
-            <div>
-              <label className="form-label inline-block mb-2 text-gray-700">
-                Alamat
-              </label>
-            </div>
-            <div className="flex">
-              <input
-                className="border border-solid border-gray-300 rounded-md w-60"
-                placeholder="  jalan kembang tulip no3"
-              />
-              <input
-                className="ml-2 text-center border border-solid border-gray-300 rounded-md w-12"
-                placeholder="RT.1"
-              />
-              <input
-                className="ml-2 border text-center border-solid border-gray-300 rounded-md w-12"
-                placeholder="RW.2"
-              />
-              <input
-                className="ml-2 border border-solid border-gray-300 rounded-md w-24 block"
-                placeholder="  Jakarta"
-              />
-            </div>
-          </div>
-          {/* No Rek */}
-          <div className="block mt-4 ml-6">
-            <div>
-              <label className="form-label inline-block mb-2 text-gray-700">
-                Nomor Rekening
-              </label>
-            </div>
-            <div className="flex">
-              <input
-                className=" border border-solid border-gray-300 rounded-md w-12"
-                placeholder=" 001"
-              />
-              <input
-                className=" ml-4 border border-solid border-gray-300 rounded-md w-52"
-                placeholder=" 7363 1616 84"
-              />
-            </div>
-            {/*Nama Rek */}
-            <div className="block mt-4 ">
+          <div className="bg-slate-400 m-6 rounded-lg h-60">
+            <div className="flex justify-left">
               <div>
-                <label className="form-label inline-block mb-2 text-gray-700">
-                  Nama Sesuai Rekening
-                </label>
+              <img
+                className=" w-48 h-48 ml-12 mt-5 rounded-full border "
+                src="http://placeimg.com/640/360/any"
+                alt="#"
+              />
+
+              <div className="block ml-9 mt-4">
+                {/* Full Name */}
+                <div>
+                  <label className="form-label inline-block mb-2 text-gray-700">
+                    Nama Lengkap
+                  </label>
+                </div>
+                <div className="flex">
+                  <input
+                    className=" border border-solid border-gray-300 rounded-md"
+                    placeholder="  Anak Ilang"
+                  />
+                  <input
+                    className=" ml-6 w-12 text-center border border-solid border-gray-300 rounded-md"
+                    placeholder="Umur"
+                  />
+                </div>
+                {/* Phone Number */}
+                <div>
+                  <label className="form-label inline-block mb-2 text-gray-700">
+                    Phone Number (+62)
+                  </label>
+                </div>
+                <div className="flex">
+                  <input
+                    className=" border border-solid border-gray-300 rounded-md"
+                    placeholder="  081213141516"
+                  />
+                </div>
+                {/* Alamat */}
+                <div>
+                  <label className="form-label inline-block mb-2 text-gray-700">
+                    Alamat
+                  </label>
+                </div>
+                <div className="flex">
+                  <input
+                    className="border border-solid border-gray-300 rounded-md w-60"
+                    placeholder="  jalan kembang tulip no3"
+                  />
+                </div>
               </div>
+                <Button className="mt-3 w-28 bg-cyan-900">Edit</Button>
+              </div>
+            </div>
+          </div>
+          {/* Assuransi */}
+          <div>
+            <div className="shadow-2xl border-b-2 m-5 rounded-lg text-center font-bold text-2xl">
+              Produk Asuransi Anda
+            </div>
+            <div className="bg-slate-400 mb-5 m-5 h-36 rounded-lg">
               <input
-                className=" border border-solid border-gray-300 rounded-md w-52"
-                placeholder=" Anak Ilang"
+                className="w-80 h-16 m-12 text-center bg-cyan-800 rounded-md text-base"
+                placeholder="Asuransi Kesehatan (BCA Assurance)"
+                disabled
+              />
+              <input
+                className="w-80 h-16 m-12 text-center bg-cyan-800 rounded-md text-base"
+                placeholder="Asuransi Jiwa (BRI Assurance)"
+                disabled
+              />
+              <input
+                className="w-80 h-16 m-12 text-center bg-cyan-800 rounded-md text-base"
+                placeholder="Asuransi Pendidikan (Mandiri Assurance)"
+                disabled
               />
             </div>
-            <Button className="mt-3 w-28 bg-cyan-900">Edit</Button>
           </div>
-        </div>
-      </div>
-      {/* Assuransi */}
-      <div>
-        <div className="shadow-2xl border-b-2 m-5 rounded-lg text-center font-bold text-2xl">
-          Produk Asuransi Anda
-        </div>
-        <div className="bg-slate-400 mb-5 m-5 h-36 rounded-lg">
-          <input
-            className="w-80 h-16 m-12 text-center bg-cyan-800 rounded-md text-base"
-            placeholder="Asuransi Kesehatan (BCA Assurance)"
-            disabled
-          />
-          <input
-            className="w-80 h-16 m-12 text-center bg-cyan-800 rounded-md text-base"
-            placeholder="Asuransi Jiwa (BRI Assurance)"
-            disabled
-          />
-          <input
-            className="w-80 h-16 m-12 text-center bg-cyan-800 rounded-md text-base"
-            placeholder="Asuransi Pendidikan (Mandiri Assurance)"
-            disabled
-          />
-        </div>
-      </div>
-      {/* Assuransi */}
-      <div>
-        <div className="shadow-2xl border-b-2 m-5 rounded-lg text-center font-bold text-2xl">
-          Produk Deposito Anda 
-        </div>
-          <CardProduk/>
-        <div className="bg-slate-400 mb-5 m-5 h-36 rounded-lg"></div>
-      </div>
+          {/* Assuransi */}
+          <div>
+            <div className="shadow-2xl border-b-2 m-5 rounded-lg text-center font-bold text-2xl">
+              Produk Deposito Anda 
+            </div>
+              <CardProduk/>
+            <div className="bg-slate-400 mb-5 m-5 h-36 rounded-lg"></div>
+          </div>
 
       <TabProfile />
       <Footer />
